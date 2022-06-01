1fa2fbacd9ca8bc9f0363bbbda94e2b547a6586d
sashiexists <myself@sashinexists.com>
2022-06-02 04:55:09 +1000

    delete unused dependencies

--- src/Shared.elm
+++ src/Shared.elm
@@ -1,7 +1,6 @@
 module Shared exposing (..)
 
 import Browser
-import Browser.Dom as Dom exposing (Viewport)
 import Browser.Navigation as Nav
 import Element exposing (Device)
 import Radical exposing (..)
